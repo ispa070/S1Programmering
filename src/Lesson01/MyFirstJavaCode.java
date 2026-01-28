9d6ef8550557f2d5bde1bb2d38075dcca63e2caf
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-01-28 10:41:58 +0100

    First commit.

---
+++ src/Lesson01/MyFirstJavaCode.java
@@ -0,0 +1,9 @@
+package Lesson01;
+
+public class MyFirstJavaCode {
+
+    public static void main(String[] args){
+        System.out.println("Hello! This is my first Java Code! Jubii!! xoxo Isak");
+    }
+
+}
