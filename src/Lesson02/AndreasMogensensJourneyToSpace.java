a8e0b751366d4118a1beaa59ea50575a8fc4b396
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-02-02 19:06:30 +0100

    Øvelse 1 udført.

---
+++ src/Lesson02/AndreasMogensensJourneyToSpace.java
@@ -0,0 +1,12 @@
+package Lesson02;
+
+public class AndreasMogensensJourneyToSpace {
+
+    public static void main(String[] args) {
+        System.out.println("Mission \t Year \t Days in space");
+        System.out.println("IRIIS \t     2015 \t 10");
+        System.out.println("Huginn \t     2023 \t 180");
+
+    }
+
+}
